effded8af24514daad0733ca68f0a6df18adc88b
masayannuu <masayannuu@gmail.com>
2018-01-27 23:59:12 +0900

    change test name

--- test/src/util/calculateHash.js
+++ test/src/util/calculateHash.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 const calculateHash = require('../../../src/util/calculateHash.js')
 const SHA256 = require('crypto-js/SHA256')
 
-describe(calculateHash.name, () => {
+describe('calculateHash', () => {
   it('return hash string', () => {
     const hash = calculateHash(1, 'data', SHA256('sample').toString(), 1517062919.271)
     assert.deepEqual(hash, 'cb256916f8c46aa6e8358232e6914a3c192762e2eca8d150bd6c8554705aacbd')
